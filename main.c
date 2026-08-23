822155a6d88d42aa5acb1e947db64c9cbb744147
vicente410 <brunottvicente@gmail.com>
2026-08-23 15:59:03 +0100

    Added lexer and parser for expressions

---
+++ main.c
@@ -0,0 +1,461 @@
+#include <stdio.h>
+#include <ctype.h>
+#define UTILS_IMPLEMENTATION
+#include "utils.h"
+
+// --- LEXER ---
+
+typedef struct {
+    String_View filename;
+    size_t line;
+    size_t col;
+} Position;
+
+typedef enum {
+    TOKEN_EOF,
+    TOKEN_ID,
+    TOKEN_COLON,
+    TOKEN_EQUAL,
+    TOKEN_COMMA,
+    TOKEN_OPEN_PAREN,
+    TOKEN_CLOSE_PAREN,
+    TOKEN_OPEN_CURLY,
+    TOKEN_CLOSE_CURLY,
+    TOKEN_RETURN,
+    TOKEN_FN,
+    TOKEN_STRUCT,
+    TOKEN_UNION,
+    TOKEN_INT_LIT,
+    TOKEN_STRING_LIT,
+    TOKEN_SEMICOLON,
+    TOKEN_OP,
+} TokenKind;
+
+typedef struct {
+    TokenKind kind;
+    union {
+        String_View id;
+        int int_lit;
+        String_View string_lit;
+        String_View op;
+    } as;
+    Position pos;
+} Token;
+
+typedef struct {
+    Token *data;
+    size_t count;
+    size_t capacity;
+} Tokens;
+
+typedef struct {
+    FILE *fp;
+    Position pos;
+    char peeked_char;
+    bool has_peeked_char;
+    Token peeked_token;
+    bool has_peeked_token;
+} Lexer;
+
+bool lexer_init(Lexer *lexer, char *filename) {
+    lexer->fp = fopen(filename, "r");
+    lexer->pos = (Position) {
+        .filename = sv_from_cstr(filename),
+        .line = 0,
+        .col = 0,
+    };
+    lexer->has_peeked_char = false;
+    lexer->has_peeked_token = false;
+
+    return lexer->fp != NULL;
+}
+
+String_View token_to_sv(Token token) {
+    String_Builder sb = {};
+    
+    switch (token.kind) {
+    case TOKEN_EOF:         sb_appendf(&sb, "TOKEN_EOF"); break;
+    case TOKEN_ID:          sb_appendf(&sb, "TOKEN_ID("SV_FMT")", SV_ARG(token.as.id)); break;
+    case TOKEN_COLON:       sb_appendf(&sb, "TOKEN_COLON"); break;
+    case TOKEN_COMMA:       sb_appendf(&sb, "TOKEN_COMMA"); break;
+    case TOKEN_OPEN_PAREN:  sb_appendf(&sb, "TOKEN_OPEN_PAREN"); break;
+    case TOKEN_CLOSE_PAREN: sb_appendf(&sb, "TOKEN_CLOSE_PAREN"); break;
+    case TOKEN_OPEN_CURLY:  sb_appendf(&sb, "TOKEN_OPEN_CURLY"); break;
+    case TOKEN_CLOSE_CURLY: sb_appendf(&sb, "TOKEN_CLOSE_CURLY"); break;
+    case TOKEN_RETURN:      sb_appendf(&sb, "TOKEN_RETURN"); break;
+    case TOKEN_FN:          sb_appendf(&sb, "TOKEN_FN"); break;
+    case TOKEN_STRUCT:      sb_appendf(&sb, "TOKEN_STRUCT"); break;
+    case TOKEN_UNION:       sb_appendf(&sb, "TOKEN_UNION"); break;
+    case TOKEN_INT_LIT:     sb_appendf(&sb, "TOKEN_INT_LIT(%d)", token.as.int_lit); break;
+    case TOKEN_STRING_LIT:  sb_appendf(&sb, "TOKEN_STRING_LIT("SV_FMT")",
+                                       SV_ARG(token.as.string_lit)); break;
+    case TOKEN_SEMICOLON:   sb_appendf(&sb, "TOKEN_SEMICOLON"); break;
+    case TOKEN_OP:          sb_appendf(&sb, "TOKEN_OP("SV_FMT")", SV_ARG(token.as.op)); break;
+    default:
+        fprintf(stderr, "UNREACHABLE\n");
+        exit(1);
+    }
+ 
+    return sv_from_sb(sb);
+}
+
+char next_char(Lexer *lexer) {
+    char ch;
+    
+    if (lexer->has_peeked_char) {
+        lexer->has_peeked_char = false;
+        ch = lexer->peeked_char;
+    } else {
+        ch = fgetc(lexer->fp);
+    }
+
+    if (ch == '\n') {
+        lexer->pos.line++;
+        lexer->pos.col = 0;
+    } else {
+        lexer->pos.col++;
+    }
+
+    return ch;
+}
+
+char peek_char(Lexer *lexer) {
+    if (!(lexer->has_peeked_char)) {
+        lexer->peeked_char = next_char(lexer);
+        lexer->has_peeked_char = true;
+    }
+    return lexer->peeked_char;
+}
+
+bool accept_char(Lexer *lexer, char ch) {
+    if (peek_char(lexer) == ch) {
+        next_char(lexer);
+        return true;
+    }
+    
+    return false;
+}
+
+Token read_id_or_keyword(Lexer *lexer) {
+    Token token = {};
+    String_Builder sb = {};
+    
+    token.pos = lexer->pos;
+
+    while (isalnum(peek_char(lexer))) {
+        da_push(&sb, next_char(lexer));
+    }
+
+    if (sv_eq_cstr(sv_from_sb(sb), "return")) {
+        token.kind = TOKEN_RETURN;
+    } else if (sv_eq_cstr(sv_from_sb(sb), "fn")) {
+        token.kind = TOKEN_FN;
+    } else if (sv_eq_cstr(sv_from_sb(sb), "struct")) {
+        token.kind = TOKEN_STRUCT;
+    } else if (sv_eq_cstr(sv_from_sb(sb), "union")) {
+        token.kind = TOKEN_UNION;
+    } else {
+        token.kind = TOKEN_ID;
+        token.as.id = sv_from_sb(sb);
+    }
+    
+    return token;
+}
+
+Token read_int_lit(Lexer *lexer) {
+    Token token = {};
+    
+    token.pos = lexer->pos;
+    
+    int int_lit = next_char(lexer) - '0';
+
+    while(isdigit(peek_char(lexer))) {
+        int_lit *= 10;
+        int_lit += next_char(lexer) - '0';
+    }
+
+    token.kind = TOKEN_INT_LIT;
+    token.as.int_lit = int_lit;
+    
+    return token;
+}
+
+Token read_string_lit(Lexer *lexer) {
+    Token token = {};
+    String_Builder sb = {};
+    char ch;
+    
+    token.pos = lexer->pos;
+    
+    next_char(lexer);
+    while ((ch = next_char(lexer)) != '"') {
+        da_push(&sb, ch);
+    }
+    
+    token.kind = TOKEN_STRING_LIT;
+    token.as.string_lit = sv_from_sb(sb);
+    
+    return token;
+}
+
+Token read_symbol(Lexer *lexer) {
+    Token token = {};
+    char ch = next_char(lexer);
+    
+    token.pos = lexer->pos;
+    
+    switch (ch) {
+    case ':': token.kind = TOKEN_COLON; break;
+    case '=': {
+        if (accept_char(lexer, '=')) {
+            token.kind = TOKEN_OP;
+            token.as.op = sv_from_cstr("==");
+        } else {
+            token.kind = TOKEN_EQUAL;
+        }
+    } break;
+    case ',': token.kind = TOKEN_COMMA; break;
+    case '(': token.kind = TOKEN_OPEN_PAREN; break;
+    case ')': token.kind = TOKEN_CLOSE_PAREN; break;
+    case '{': token.kind = TOKEN_OPEN_CURLY; break;
+    case '}': token.kind = TOKEN_CLOSE_CURLY; break;
+    case ';': token.kind = TOKEN_SEMICOLON; break;
+    default:
+        token.kind = TOKEN_OP;
+        String_Builder sb = {};
+        da_push(&sb, ch);
+        while (strchr("+-*/!=<>", peek_char(lexer)) != NULL) {
+            da_push(&sb, next_char(lexer));
+        }
+        token.as.op = sv_from_sb(sb);
+    }
+    
+    return token;
+}
+
+Token next_token(Lexer *lexer) {
+    if (lexer->has_peeked_token) {
+        lexer->has_peeked_token = false;
+        return lexer->peeked_token;
+    }
+
+    while (peek_char(lexer) == ' ' || peek_char(lexer) == '\n' || peek_char(lexer) == '\t') {
+        next_char(lexer);
+    }
+
+    if (feof(lexer->fp)) {
+    	return (Token) { .kind = TOKEN_EOF };
+    } else if (isalpha(peek_char(lexer))) {
+        return read_id_or_keyword(lexer);
+    } else if (isdigit(peek_char(lexer))) {
+        return read_int_lit(lexer);
+    } else if (peek_char(lexer) == '"') {
+        return read_string_lit(lexer);
+    } else {
+    	return read_symbol(lexer);
+    }
+}
+
+Token peek_token(Lexer *lexer) {
+    if (!(lexer->has_peeked_token)) {
+        lexer->peeked_token = next_token(lexer);
+        lexer->has_peeked_token = true;
+    }
+    return lexer->peeked_token;
+}
+
+void expect_token(Lexer *lexer, TokenKind kind) {
+    if (next_token(lexer).kind != kind) {
+        fprintf(stderr, "Error: Unexepected token");
+        exit(1);
+    }
+}
+
+bool accept_token(Lexer *lexer, TokenKind kind) {
+    if (peek_token(lexer).kind == kind) {
+        next_token(lexer);
+        return true;
+    }
+    
+    return false;
+}
+
+// --- PARSER ---
+
+struct Ast;
+
+typedef struct {
+    struct Ast **data;
+    size_t count;
+    size_t capacity;
+} Asts;
+
+typedef enum {
+    AST_ID,
+    AST_INT_LIT,
+    AST_CALL,
+    AST_OP,
+} AstKind;
+
+typedef struct {
+    String_View op;
+    struct Ast *lhs;
+    struct Ast *rhs;
+} Op;
+
+typedef struct {
+    struct Ast *fn;
+    Asts args;
+} Call;
+
+typedef struct Ast {
+    AstKind kind;
+    union {
+        String_View id;
+        int int_lit;
+        Op op;
+        Call call;
+    } as;
+} Ast;
+
+String_View ast_to_sv(Ast *ast) {
+    String_Builder sb = {};
+    
+    switch (ast->kind) {
+    case AST_ID:           da_append(&sb, &ast->as.id); break;
+    case AST_INT_LIT:      sb_appendf(&sb, "%d", ast->as.int_lit); break;
+    case AST_OP: {
+        String_View lhs_sv = ast_to_sv(ast->as.op.lhs);
+        String_View rhs_sv = ast_to_sv(ast->as.op.rhs);
+        sb_appendf(
+            &sb,
+            "("SV_FMT" "SV_FMT" "SV_FMT")",
+            SV_ARG(lhs_sv),
+            SV_ARG(ast->as.op.op),
+            SV_ARG(rhs_sv)
+        );
+    } break;
+    case AST_CALL: {
+        String_View fn_sv = ast_to_sv(ast->as.call.fn);
+        sb_appendf(&sb, SV_FMT, SV_ARG(fn_sv));
+        sb_appendf(&sb, "(");
+        
+        for (size_t i = 0; i < ast->as.call.args.count; i++) {
+            if (i != 0) sb_appendf(&sb, ", ");
+            String_View arg_sv = ast_to_sv(ast->as.call.args.data[i]);
+            sb_appendf(&sb, SV_FMT, SV_ARG(arg_sv));
+        }
+        
+        sb_appendf(&sb, ")");
+    } break;
+    default:
+        fprintf(stderr, "UNREACHABLE\n");
+        exit(1);
+    }
+    
+    return sv_from_sb(sb);
+}
+
+Ast *parse_expr(Lexer *lexer, size_t precedence);
+
+Ast *parse_factor(Lexer *lexer) {
+    Ast *result;
+    Token token = next_token(lexer);
+    
+    if (token.kind == TOKEN_ID) {
+        result = malloc(sizeof(Ast));
+        result->kind = AST_ID;
+        result->as.id = token.as.id;
+    } else if (token.kind == TOKEN_INT_LIT) {
+        result = malloc(sizeof(Ast));
+        result->kind = AST_INT_LIT;
+        result->as.int_lit = token.as.int_lit;
+    } else if (token.kind == TOKEN_OPEN_PAREN) {
+        result = parse_expr(lexer, 0);
+        expect_token(lexer, TOKEN_CLOSE_PAREN);
+    } else {
+        fprintf(stderr, "Error: syntax error\n");
+        exit(1);
+    }
+    
+    if (accept_token(lexer, TOKEN_OPEN_PAREN)) {
+        Ast *new_result = malloc(sizeof(Ast));
+        new_result->kind = AST_CALL;
+        new_result->as.call.fn = result;
+        result = new_result;
+        
+        while (!accept_token(lexer, TOKEN_CLOSE_PAREN)) {
+            da_push(&result->as.call.args, parse_expr(lexer, 0));
+            if (peek_token(lexer).kind != TOKEN_CLOSE_PAREN) {
+                expect_token(lexer, TOKEN_COMMA);
+            }
+        }
+    }
+
+    return result;
+}
+
+Ast *parse_expr(Lexer *lexer, size_t precedence) {
+    Ast *result = precedence == 3 ? parse_factor(lexer)
+                                  : parse_expr(lexer, precedence + 1);
+    
+    while (peek_token(lexer).kind == TOKEN_OP && (
+        (precedence == 2 && (
+            sv_eq_cstr(peek_token(lexer).as.op, "*") ||
+            sv_eq_cstr(peek_token(lexer).as.op, "/"))) ||
+        (precedence == 1 && (
+            sv_eq_cstr(peek_token(lexer).as.op, "+") ||
+            sv_eq_cstr(peek_token(lexer).as.op, "-"))) ||
+        (precedence == 0 && (
+            sv_eq_cstr(peek_token(lexer).as.op, "<") ||
+            sv_eq_cstr(peek_token(lexer).as.op, ">") ||
+            sv_eq_cstr(peek_token(lexer).as.op, "<=") ||
+            sv_eq_cstr(peek_token(lexer).as.op, ">=") ||
+            sv_eq_cstr(peek_token(lexer).as.op, "==") ||
+            sv_eq_cstr(peek_token(lexer).as.op, "!=")))
+    )) {
+        Ast *new_result = malloc(sizeof(Ast));
+        new_result->kind = AST_OP;
+        new_result->as.op.lhs = result;
+        new_result->as.op.op = next_token(lexer).as.op;
+        new_result->as.op.rhs = parse_expr(lexer, precedence + 1);
+        result = new_result;
+    }
+
+    return result;
+}
+
+// --- MAIN ---
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s [filename]\n", argv[0]);
+        exit(1);
+    }
+
+    char* program_name = argv[1];
+
+    Lexer lexer = {};
+    lexer_init(&lexer, program_name);
+    Ast *ast = parse_expr(&lexer, 0);
+    
+    String_View ast_sv = ast_to_sv(ast);
+    printf(SV_FMT"\n", SV_ARG(ast_sv));
+    
+    /*for (size_t i = 0; i < asts.count; i++) {
+        String_View ast_sv = ast_to_sv(asts.data[i]);
+        printf(SV_FMT"\n", SV_ARG(ast_sv));
+    }*/
+
+    /*while (true) {
+        Token token = next_token(&lexer);
+        if (token.kind == TOKEN_EOF) break;
+        String_View token_sv = token_to_sv(token);
+        printf(SV_FMT"\n", SV_ARG(token_sv));
+    }*/
+    //Stmt stmt = parse_stmt(&lexer);
+    //String_View stmt_sv = stmt_to_sv(stmt);
+    //printf(SV_FMT"\n", SV_ARG(stmt_sv));
+
+    return 0;
+}
---
+++ utils.h
@@ -0,0 +1,349 @@
+#include <assert.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// TODO: Implement parser combinators using String_View
+
+// Dynamic Array
+
+#define da_reserve(da, additional)\
+    do {\
+        if (((da)->count + additional) > (da)->capacity) {\
+            if ((da)->capacity == 0) {\
+                (da)->capacity = 1;\
+            }\
+            while (((da)->count + additional) > (da)->capacity) {\
+                (da)->capacity *= 2;\
+            }\
+            (da)->data = realloc((da)->data, (da)->capacity * sizeof(*(da)->data));\
+        }\
+    } while (0)
+
+#define da_insert(da, index, value)\
+    do {\
+        da_reserve((da), 1);\
+        memmove((da)->data + (index) + 1, (da)->data + (index),\
+        ((da)->count++ - (index)) * sizeof(*(da)->data));\
+        (da)->data[index] = (value);\
+    } while (0)
+
+#define da_remove(da, index)\
+    do {\
+        memmove((da)->data + (index), (da)->data + (index) + 1,\
+        ((da)->count-- - (index)) * sizeof(*(da)->data));\
+    } while (0)
+
+#define da_push(da, value)\
+    do {\
+        da_reserve((da), 1);\
+        (da)->data[(da)->count++] = (value);\
+    } while (0)
+
+#define da_append_raw(da, other, other_count)\
+    do {\
+        da_reserve((da), (other_count));\
+        memcpy((da)->data + (da)->count, (other), (other_count) * sizeof(*(da)->data));\
+        (da)->count += (other_count);\
+    } while (0)
+
+#define da_append(da, other) da_append_raw((da), (other)->data, (other)->count)
+
+#define da_at(da, i) (da)->data[assert((da)->count > i), i]
+#define da_first(da) (da)->data[assert((da)->count > 0), 0]
+#define da_last(da) (da)->data[assert((da)->count > 0), (da)->count - 1]
+#define da_pop(da) (da)->data[assert((da)->count > 0), --(da)->count]
+#define da_foreach(x, da) for (typeof((da)->data[0]) *x = (da)->data; x < (da)->data + (da)->count; x++)
+
+// String Builder and View
+
+typedef struct {
+    char* data;
+    size_t count;
+    size_t capacity;
+} String_Builder;
+
+typedef struct {
+    const char* data;
+    size_t count;
+} String_View;
+
+#define SV_FMT "%.*s"
+#define SV_ARG(sv) (int)(sv).count, (sv).data
+
+int sb_appendf(String_Builder *sb, const char *fmt, ...);
+bool sb_read_file(String_Builder *sb, const char *path);
+
+String_View sv_from_parts(const char *cstr, size_t count);
+String_View sv_from_cstr(const char *cstr);
+
+bool sv_eq(String_View a, String_View b);
+bool sv_eq_cstr(String_View sv, char *cstr);
+
+bool sv_starts_with(String_View sv, String_View start);
+bool sv_ends_with(String_View sv, String_View end);
+
+int sv_find_char(String_View sv, char pattern);
+int sv_find_pred(String_View sv, int (*pattern)(int));
+int sv_find_sv(String_View sv, String_View pattern);
+int sv_find_cstr(String_View sv, char *pattern);
+
+int sv_find_nth_char(String_View sv, char pattern, int nth);
+int sv_find_nth_pred(String_View sv, int (*pattern)(int), int nth);
+int sv_find_nth_sv(String_View sv, String_View pattern, int nth);
+int sv_find_nth_cstr(String_View sv, char *pattern, int nth);
+
+String_View sv_slice(String_View sv, size_t start, size_t end);
+String_View sv_slice_start(String_View sv, size_t start);
+String_View sv_slice_end(String_View sv, size_t end);
+
+String_View sv_split_at(String_View *sv, size_t n);
+String_View sv_rsplit_at(String_View *sv, size_t n);
+String_View sv_split_delim(String_View *sv, char delim);
+String_View sv_rsplit_delim(String_View *sv, char delim);
+
+String_View sv_trim(String_View sv);
+String_View sv_trim_start(String_View sv);
+String_View sv_trim_end(String_View sv);
+
+#ifdef UTILS_IMPLEMENTATION
+int isnotspace(int c) {
+    return c != ' ' && c != '\t' && c != '\n';
+}
+
+int sb_appendf(String_Builder *sb, const char *fmt, ...) {
+    va_list args;
+
+    va_start(args, fmt);
+    int n = vsnprintf(NULL, 0, fmt, args);
+    va_end(args);
+
+    da_reserve(sb, n + 1);
+    char *dest = sb->data + sb->count;
+
+    va_start(args, fmt);
+    vsnprintf(dest, n+1, fmt, args);
+    va_end(args);
+
+    sb->count += n;
+
+    return n;
+}
+
+bool sb_read_file(String_Builder *sb, const char *path) {
+    FILE *fp = fopen(path, "r");
+    if (!fp) return -1;
+
+    fseek(fp, 0, SEEK_END);
+    int n = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+
+    da_reserve(sb, n);
+    fread(sb->data + sb->count, n, 1, fp);
+    sb->count += n;
+
+    return 0;
+}
+
+String_View sv_from_parts(const char *data, size_t count) {
+    return (String_View){
+        .data = data,
+        .count = count,
+    };
+}
+
+String_View sv_from_sb(String_Builder sb) {
+    return sv_from_parts(sb.data, sb.count);
+}
+
+String_View sv_from_cstr(const char *cstr) {
+    size_t len = 0;
+
+    while (*(cstr + len) != '\0') len++;
+
+    return sv_from_parts(cstr, len);
+}
+
+bool sv_eq(String_View a, String_View b) {
+    if (a.count != b.count) return false;
+
+    for (size_t i = 0; i < a.count; i++) {
+        if (a.data[i] != b.data[i]) return false;
+    }
+
+    return true;
+}
+
+bool sv_eq_cstr(String_View sv, char *cstr) {
+    for (size_t i = 0; i < sv.count; i++) {
+        if (cstr[i] == '\0' || sv.data[i] != cstr[i]) return false;
+    }
+
+    return cstr[sv.count] == '\0';
+}
+
+bool sv_starts_with(String_View sv, String_View start) {
+    return sv_eq(sv_slice_start(sv, start.count), start);
+}
+
+bool sv_ends_with(String_View sv, String_View end) {
+    return sv_eq(sv_slice_end(sv, sv.count - end.count), end);
+}
+
+int sv_find_char(String_View sv, char pattern) {
+    return sv_find_nth_char(sv, pattern, 1);
+}
+
+int sv_find_pred(String_View sv, int (*pattern)(int)) {
+    return sv_find_nth_pred(sv, pattern, 1);
+}
+
+int sv_find_sv(String_View sv, String_View pattern) {
+    return sv_find_nth_sv(sv, pattern, 1);
+}
+
+int sv_find_cstr(String_View sv, char *pattern) {
+    return sv_find_nth_cstr(sv, pattern, 1);
+}
+
+int sv_find_nth_char(String_View sv, char pattern, int nth) {
+    int count = 0;
+
+    if (nth == 0) return -1;
+
+    if (nth > 0) {
+        for (size_t i = 0; i < sv.count; i++) {
+            if (sv.data[i] == pattern) {
+                count++;
+                if (count == nth) return i;
+            }
+        }
+    } else {
+        for (int i = sv.count - 1; i >= 0; i--) {
+            if (sv.data[i] == pattern) {
+                count--;
+                if (count == nth) return i;
+            }
+        }
+    }
+
+    return -1;
+}
+
+int sv_find_nth_pred(String_View sv, int (*pattern)(int), int nth) {
+    int count = 0;
+
+    if (nth == 0) return -1;
+
+    if (nth > 0) {
+        for (size_t i = 0; i < sv.count; i++) {
+            if (pattern(sv.data[i])) {
+                count++;
+                if (count == nth) return i;
+            }
+        }
+    } else {
+        for (int i = sv.count - 1; i >= 0; i--) {
+            if (pattern(sv.data[i])) {
+                count--;
+                if (count == nth) return i;
+            }
+        }
+    }
+
+    return -1;
+}
+
+int sv_find_nth_sv(String_View sv, String_View pattern, int nth) {
+    int count = 0;
+
+    if (nth == 0 || pattern.count == 0) return -1;
+
+    if (nth > 0) {
+        for (size_t i = 0; i < sv.count - pattern.count + 1; i++) {
+            if (sv_starts_with(sv_slice_end(sv, i), pattern)) {
+                count++;
+                if (count == nth) return i;
+            }
+        }
+    } else {
+        for (int i = sv.count; i > (int)pattern.count - 1; i--) {
+            if (sv_ends_with(sv_slice_start(sv, i), pattern)) {
+                count--;
+                if (count == nth) return i - pattern.count;
+            }
+        }
+    }
+
+    return -1;
+}
+
+int sv_find_nth_cstr(String_View sv, char* pattern, int nth) {
+    return sv_find_nth_sv(sv, sv_from_cstr(pattern), nth);
+}
+
+String_View sv_slice(String_View sv, size_t start, size_t end) {
+    return sv_from_parts(sv.data + start, end - start);
+}
+
+String_View sv_slice_start(String_View sv, size_t end) {
+    return sv_slice(sv, 0, end);
+}
+
+String_View sv_slice_end(String_View sv, size_t start) {
+    return sv_slice(sv, start, sv.count);
+}
+
+String_View sv_split_at(String_View *sv, size_t n) {
+    String_View result = sv_from_parts(sv->data, n);
+    sv->data += n;
+    sv->count -= n;
+    return result;
+}
+
+String_View sv_rsplit_at(String_View *sv, size_t n) {
+    String_View result = sv_from_parts(sv->data + n, sv->count - n);
+    sv->count = n;
+    return result;
+}
+
+String_View sv_split_delim(String_View *sv, char delim) {
+    int pos = sv_find_char(*sv, delim);
+    String_View result = *sv;
+
+    if (pos < 0) {
+        *sv = (String_View){};
+    } else {
+        result = sv_slice_start(sv_split_at(sv, pos + 1), pos);
+    }
+
+    return result;
+}
+
+String_View sv_rsplit_delim(String_View *sv, char delim) {
+    int pos = sv_find_nth_char(*sv, delim, -1);
+    String_View result = *sv;
+
+    if (pos < 0) {
+        *sv = (String_View){};
+    } else {
+        result = sv_slice_end(sv_rsplit_at(sv, pos), 1);
+    }
+
+    return result;
+}
+
+String_View sv_trim(String_View sv) {
+    return sv_trim_end(sv_trim_start(sv));
+}
+
+String_View sv_trim_start(String_View sv) {
+    return sv_slice_end(sv, sv_find_pred(sv, isnotspace));
+}
+
+String_View sv_trim_end(String_View sv) {
+    return sv_slice_start(sv, sv_find_nth_pred(sv, isnotspace, -1) + 1);
+}
+#endif
